d524de90af92419f04b49a2edd345a0c0753a171
Kyle Lizardo <kyleethan442@gmail.com>
2024-09-14 10:47:55 +0800

    update

--- app/src/main/java/com/example/poscanteen/MainActivity.java
+++ app/src/main/java/com/example/poscanteen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.poscanteen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signin = findViewById(R.id.signinBtn);
+        signin.setOnClickListener(e -> {
+            Intent intent = new Intent(MainActivity.this, checkout.class);
+            startActivity(intent);
+        });
     }
+
+
 }
--- app/src/main/java/com/example/poscanteen/checkout.java
+++ app/src/main/java/com/example/poscanteen/checkout.java
@@ -1,4 +1,24 @@
 package com.example.poscanteen;
 
-public class checkout {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class checkout extends AppCompatActivity {
+
+    private Button signin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.checkout);
+    }
+
 }
