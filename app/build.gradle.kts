607f13582f2a712609032f8596b919beb17f3928
LAURENCE <wowenzzz0801@gmail.com>
2024-09-17 17:29:21 +0800

    asdasdas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.poscanteen"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -41,3 +41,19 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
+
--- app/src/main/java/com/example/poscanteen/MainActivity.java
+++ app/src/main/java/com/example/poscanteen/MainActivity.java
@@ -11,32 +11,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button login;
     private TextView register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        FirebaseApp.initializeApp(this);
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
 
         login = findViewById(R.id.loginBtn);
         login.setOnClickListener(e -> {
             Intent intent = new Intent(MainActivity.this, home.class);
             startActivity(intent);
         });
+
+
         register = findViewById(R.id.regtxt);
         register.setOnClickListener(e -> {
             Intent intent = new Intent(MainActivity.this, register.class);
             startActivity(intent);
         });
     }
-
-
 }
